3e572dc6f67bddc9207c1e870ef52a41e9d49fa4
victoria.piassa <victoriapiassa2@gmail.com>
2025-07-14 22:10:57 -0300

    feat: implementar conexão com o banco de dados e adicionar modelo de usuário

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,11 +1,11 @@
-import "dotenv/config";
-import mongoose from "mongoose";    
+import "dotenv/config"; // importação das variaveis de ambiente do arquivo .env
+import mongoose from "mongoose";     // importação do mongoose para conectar ao MongoDB
 
 const { MONGO_URL } = process.env;
 
-export async function connectDB() {
+export async function connectDB() { //função para conectar ao banco de dados 
     try {
-        await mongoose.connect(MONGO_URL);
+        await mongoose.connect(MONGO_URL);  // mongoose é um biblioteca do node para conectar ao Mongo que organiza, valida e manipula os dados 
         console.log('Deu certo a conexão com o banco de dados!');
     } catch (error) {
         console.error('Erro ao conectar com o banco de dados:', error);
--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,39 @@
- import 'dotenv/config';
- import express from "express";
- import { connectDB } from "./config/db.js"; // sem ou com js?
+ import 'dotenv/config'; // importa dotenv(.env) para utilizar as variaveis de ambiente
+ import express from "express"; // importa o express(fremerwork) para criar o servidor
+ import { connectDB } from "./config/db.js"; // importa a função(connectDB) para conectar ao banco de dados
+ import User from './models/user.js'; // importa o modelo de usuário(User) 
 
-const app = express();
-const { PORT, MONGO_URL } = process.env; 
+const app = express(); // cria uma instância do express
+const { PORT } = process.env; // define a porta do servidor a partir das variáveis de ambiente
 
+ app.get("/users", async (_req, res) => {  // rota para obter todos os usuários
+    connectDB();
+    try {
+        const userDoc = await User.find(); // busca todos os usuários no banco de dados
+        res.json(userDoc);  // retorna os usuários encontrados em formato JSON
+    }catch (error) {
+        res.status(500).json(error); // retorna um erro 404 se não encontrar usuários
+    }
+    
+    
+});
 
+app.post("/users", async (req, res) => { // rota para criar um novo usuário
+    connectDB();
 
- app.get('/', (_req, res) => {
-    res.json("Olá, mundo!");    
+    try {
+        const newUserDoc = await User.create({
+        name: "Teste",
+        email: "teste@teste.com",
+        password: "12345678910",
+        }); 
+
+        res.json(newUserDoc); // retorna o novo usuário criado em formato JSON
+
+    }catch (error) {
+        res.status(500).json(error)
+
+    }
 });
 
 app.listen(PORT, () => {
