7fb44726e129c0b45c7528a61b0c72ca2271a52f
victoria.piassa <victoriapiassa2@gmail.com>
2025-09-13 12:50:24 -0300

    registro dos filmes do mongo

---
+++ back-end/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import filmRoutes from "./domains/users/films/routerFilm.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// usa as rotas
+app.use("/filmes", filmRoutes);
+
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+});
