cd05af4fd588196f683f7d71026eee0bac0de83c
victoria.piassa <victoriapiassa2@gmail.com>
2025-10-20 06:38:08 -0300

    alteração no homeAdmin

--- src/pages/admin/homeAdmin.vue
+++ src/pages/admin/homeAdmin.vue
@@ -1,4 +1,11 @@
 <template>
 
-    <div class="bg-red-200">  <p> Olá </p></div>
+    <main
+      class="flex-1 mt-5 m-5 min-h-[calc(90vh-2rem)] bg-gray-100 flex items-center justify-center p-8"
+    >
+
+    <p> Olá, Bem vindo </p>
+      
+    </main>
+
 </template>
