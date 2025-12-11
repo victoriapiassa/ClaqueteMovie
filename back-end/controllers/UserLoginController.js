7781f1ecdfbd9390d818dfb6a0b5e11069146878
victoria.piassa <victoriapiassa2@gmail.com>
2025-12-10 21:58:18 -0300

    criação do controller do UserLogin

---
+++ back-end/controllers/UserLoginController.js
@@ -0,0 +1,9 @@
+import { connectDB } from '../config/db.js';
+import User from '../domains/users/model.js';
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
+
+
+class UserLoginController {
+    
+}
